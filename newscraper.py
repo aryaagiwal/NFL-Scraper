58dcd86ef7a948d97e745545d09640218831804c
Om Goswami <omgoswami101@gmail.com>
2022-03-30 16:01:37 -0500

    new scraper file prints all necessary info to stdout, last commit was empty

---
+++ newscraper.py
@@ -0,0 +1,28 @@
+import bs4 as bs
+from urllib.request import Request, urlopen
+
+if __name__ == "__main__":
+    req = Request("https://www.nfl.com/standings/division/2020/REG")
+    web_page = urlopen(req).read()
+    soup = bs.BeautifulSoup(web_page, 'lxml')
+    division_names = [table.find("th", {"aria-label" : "Division name"}).get_text()
+                      for table in soup.find_all("table")]
+    names = []
+    wins = []
+    losses = []
+    for table in soup.find_all("table"):
+        for body in table.find_all("tbody"):
+            for row in table.find_all("tr"):
+                #names.append(row.find("div", {"class" : "d3-o-club-fullname"}).get_text())
+                for td in row.find_all("td"):
+                    print(td.get_text())
+            '''
+            for a in body.find_all("a", {"class" : "d3-o-club-info"}):
+
+                print(a.find("div", {"class" : "d3-o-club-fullname"}).get_text())
+            tds = [body.find_all("td")]
+            print(tds)'''
+                #body.find("div", {"class" : "d3-o-club-fullname"}).get_text)
+            #team_dict[body.find("div", {"class" : "d3-o-club-fullname"}).get_text] = body.find("td").get_text()
+    #print(team_dict)
+
